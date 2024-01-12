056c7215f470043837308dfcfe0ba19211e93f44
Milica Vasiljevic <milica.vasiljevic@vega.local>
2024-01-12 11:34:24 +0100

    login form

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import {Button} from "@mui/material";
+import Loginform from './components/login/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <Loginform />
+    </div>
+  );
+}
+
+export default App;
