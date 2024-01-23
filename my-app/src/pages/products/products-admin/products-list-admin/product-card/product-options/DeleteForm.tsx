08d91216fe165c59c0ae2a69e1dfa343a7ba23b5
Milica Vasiljevic <milica.vasiljevic@vega.local>
2024-01-23 10:08:43 +0100

    change

--- my-app/src/pages/products/products-admin/products-list-admin/product-card/product-options/DeleteForm.tsx
+++ my-app/src/pages/products/products-admin/products-list-admin/product-card/product-options/DeleteForm.tsx
@@ -8,31 +8,30 @@ const DeleteForm: React.FC<DeleteFormProps> = ({ product, onDelete }) => {
 
     const productHttp = new ProductHttp()
 
-    const {close} = useModalContext()
+    const { close } = useModalContext()
 
-    const deleteProduct = async() => {
-        try{
+    const deleteProduct = async () => {
+        try {
             await productHttp.deleteProduct(product.id);
             onDelete(product.id);
             close();
         }
-        catch(error){
-            console.log(error);
+        catch (error) {
             alert("Error");
         }
     }
 
-  return (
-    <div>
-        <p>Are you sure you want to delete {product.name}?</p>
-        <Button onClick={deleteProduct} style={{ backgroundColor: 'rgb(214, 129, 1)' , marginLeft:'90px', marginTop: '20px', marginBottom: '20px', marginRight:'5px' }} variant="contained" color="primary">
-            Yes
-        </Button>
-        <Button onClick={close} style={{ backgroundColor: 'rgb(214, 129, 1)' , marginTop: '20px', marginBottom: '20px' , marginLeft:'5px'}} variant="contained" color="primary">
-            No
-        </Button>
-    </div>
-  );
+    return (
+        <div>
+            <p>Are you sure you want to delete {product.name}?</p>
+            <Button onClick={deleteProduct} style={{ backgroundColor: 'rgb(214, 129, 1)', marginLeft: '90px', marginTop: '20px', marginBottom: '20px', marginRight: '5px' }} variant="contained" color="primary">
+                Yes
+            </Button>
+            <Button onClick={close} style={{ backgroundColor: 'rgb(214, 129, 1)', marginTop: '20px', marginBottom: '20px', marginLeft: '5px' }} variant="contained" color="primary">
+                No
+            </Button>
+        </div>
+    );
 }
 
 export default DeleteForm
